3ce4ef1d2b26b4f3f1d87454e588b17818a6906a
David Chawei Hsu <chawei.hsu@gmail.com>
2011-02-27 18:50:37 -0800

    include movies app

--- moovy/settings.py
+++ moovy/settings.py
@@ -87,6 +87,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'movies',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
