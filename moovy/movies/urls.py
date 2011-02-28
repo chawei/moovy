9c48cca324585f33f9cc9551d0f72bb0b9fbb239
David Chawei Hsu <chawei.hsu@gmail.com>
2011-02-27 19:46:32 -0800

    fix routing

--- moovy/movies/urls.py
+++ moovy/movies/urls.py
@@ -1,13 +1,5 @@
-import views
+from django.conf.urls.defaults import *
 
-urlpatterns = (
-    (r'^movies/$', 'views.index'),
-    # Example:
-    # (r'^moovy/', include('moovy.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('moovy.movies.views',
+    (r'^$', 'index'),
 )
--- moovy/urls.py
+++ moovy/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls.defaults import *
-from moovy.movies import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^movies/$', 'movies.views.index'),
+    (r'^movies/$', include('moovy.movies.urls')),
     # Example:
-    # (r'^moovy/', include('movies.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
